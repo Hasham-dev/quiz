c683ab6d230b029ab39f4dcae145ef8d1664d425
Hasham-dev <vakanihasham64@gmail.com>
2020-08-12 01:22:33 +0500

    Quiz App

--- public/app.js
+++ public/app.js
@@ -1,85 +1,73 @@
-// console.log('Hello World');
-
-///Checking Objects
-
-
-// var bioData = {
-//     firstName: 'Hasham',
-//     lastName: 'Minhas',
-//     surName: 'Vakani'
-// }
-
-// console.log(`Hey ! my name is ${bioData.firstName} ${bioData.lastName} ${bioData.surName}`);
-
-
-// using javascript contructor
-
-
-// function Students(roll,name){
-//     this.name = name;
-//     this.roll = roll;
-// }
-
-
-// var studentOne = new Students('Hasham',001);
-// var studentTwo = new Students('Usman',002);
-
-
-// Testing prototypes
-
-
-// function Students(name ,roll){
-//     this.name = name;
-//     this.roll = roll;
-
-
-    // this.getName = function(){
-    //     return this.name;
-    // }
-// }
-// Demo prototype
-
-// Students.prototype.getName = function(){
-//     return this.name
-// }
-
-// var studentOne = new Students('Hasham',001);
-// var studentTwo = new Students('Usman',002);
-
-// console.log(studentOne);
-
-
-// Finding property using "IN"
-// console.log( 'roll' in studentTwo);
-
-
-// returning Object property
-// for (var prop in studentTwo){
-//     console.log(prop);
-// }
-
-// return prop with value
-// for (var prop in studentTwo){
-//     console.log(prop,studentTwo[prop]);
-// }
-
-
-
-//// Making Firebase database
-
-function saveData(){
-    var name = document.getElementById('name');
-    var roll = document.getElementById('roll');
-
-    var student = {
-        name: name.value,
-        roll: roll.value
-    }
-
-    console.log(`Student Name is ${student.name} & roll was ${student.roll}`);
-
-
-    // adding data
-    firebase.database().ref('Student/student1').push(student)
-
+var question =[{
+    "question": "Who designed Pakistan's national flag ?",
+    "option1": "Fatima Jinnah",
+    "option2": "Ameer-ud-din Khidwai",
+    "option3": "Wali Khan",
+    "option4": "Tikka Khan",
+    "answer": "2"
 }
+,{
+    "question": "On 14th August which events are occured ?",
+    "option1": "Flag-raising ceremonies",
+    "option2": "Parades",
+    "option3": "Cultural Events",
+    "option4": "All Above",
+    "answer": "4"
+},{
+    "question": "What is meaning of Pakistan ?",
+    "option1": "Muslim Land",
+    "option2": "Land of five rivers",
+    "option3": "Desert",
+    "option4": "Holy Land",
+    "answer": "4"
+},{
+    "question": "Who wrote Pakistan's first national anthem ?",
+    "option1": "Hafeez Jallundhari",
+    "option2": "Abdul Rab Nishtar",
+    "option3": "Jagannath Azad",
+    "option4": "Ahmed Chagla",
+    "answer": "1"
+},{
+    "question": "Which party was in power in North West Frontier Proveince at the time of independence",
+    "option1": "Muslim League",
+    "option2": "Congress",
+    "option3": "Justice Party",
+    "option4": "Communist Party",
+    "answer": "2"
+},{
+    "question": "On 14th August 1947, there was ?",
+    "option1": "7 Ramadan",
+    "option2": "15 Ramadan",
+    "option3": "21 Ramadan",
+    "option4": "27 Ramadan",
+    "answer": "4"
+},{
+    "question": "Which is the national Language of Pakistan ?",
+    "option1": "Hindi",
+    "option2": "Bengali",
+    "option3": "Tamil",
+    "option4": "Urdu",
+    "answer": "4"
+},{
+    "question": "Which one of these is also known as the Father of the Nation ?",
+    "option1": "Mohammad Ali Jinnah",
+    "option2": "Maulana Shaukat Ali",
+    "option3": "Mohammad Ali Jouhar",
+    "option4": "Muhammad Iqbal",
+    "answer": "1"
+},{
+    "question": "The Pakistan Movement was led by which political Party ?",
+    "option1": "Pakistan Muslim League",
+    "option2": "Pakistan People Party",
+    "option3": "All-India People's Party",
+    "option4": "All-India Muslim League",
+    "answer": "4"
+},{
+    "question": "Which one of these was the first Governor General of Pakistan ?",
+    "option1": "Liaquat Ali Khan",
+    "option2": "Mohammad Ali Jinnah",
+    "option3": "Iskander Mirza",
+    "option4": "Mohammad Ali Bagora",
+    "answer": "1"
+}
+]
